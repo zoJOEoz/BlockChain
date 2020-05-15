237bd18aadf5d55612d30e804ea5db73cf4c3728
zoJOEoz <61220493+zoJOEoz@users.noreply.github.com>
2020-05-15 12:40:36 +0800

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,21 @@
+class Blockchain(object):
+	def __init__(self):
+		self.chain =1
+		self.current_transactions=[]
+
+	def new_block(self):
+		# NEW creates a  new blockchain and add it to the chain
+		self=2
+		
+		pass
+	def new_transaction(self):
+		# Adds a new transation to the list of transaction
+		pass
+	@staticmethod
+		def hash(block)
+		# hashes a block
+		pass
+	@property
+	def last_block(self):
+		#returns the last block in the chain
+		pass
